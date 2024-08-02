1ec84e259329c6291e243551c7cc916306c9c607
Sergio del Campo Puerta <sergio@sergioelmoreno.com>
2024-08-02 18:07:50 +0200

    NewGenreForm done

--- src/components/Forms/NewGenreForm.jsx
+++ src/components/Forms/NewGenreForm.jsx
@@ -2,10 +2,12 @@ import axios from "axios"
 import { useState } from "react"
 import { Button, Col, Form, Row } from "react-bootstrap"
 import { useNavigate } from "react-router-dom"
+
 const API_URL = "http://localhost:5005"
 
 
 const NewGenreForm = () => {
+
     const [formValues, setFormValues] = useState({
         name: "",
         origins: {},
@@ -41,15 +43,19 @@ const NewGenreForm = () => {
             })
     }
 
-    const handleSubmit = (event) => {
-        event.preventDefault()
-        const countriesArray = originsValues.countries.split(",")
+    const generateAndCleanArray = arr => arr.split(",").map(el => el.trim())
 
-        countriesArray.forEach(el => el.trim())
-        originsValues.countries = countriesArray
-        formValues.origins = originsValues
+    const handleSubmit = event => {
+
+        event.preventDefault()
+
+        formValues.origins = generateAndCleanArray(originsValues.countries)
+        formValues.linkedBands = generateAndCleanArray(formValues.linkedBands)
+        formValues.childrenGenres = generateAndCleanArray(formValues.childrenGenres)
+        formValues.images = generateAndCleanArray(formValues.images)
 
         const requestBody = formValues
+
         axios
             .post(`${API_URL}/genres/`, requestBody)
             .then(res => navigate('/genres'))
@@ -58,7 +64,9 @@ const NewGenreForm = () => {
 
     return (
         <div className="NewGenreForm" >
+
             <Form onSubmit={handleSubmit}>
+
                 <Form.Group className="mb-3" controlId="nameId">
                     <Form.Label>Name:<sup>*</sup></Form.Label>
                     <Form.Control
@@ -66,34 +74,39 @@ const NewGenreForm = () => {
                         type="text"
                         placeholder="Enter name"
                         name="name"
-                        value={formValues.name} />
+                        value={formValues.name}
+                        required />
                 </Form.Group>
+
                 <p className="fw-bold text-uppercase">Origins:</p>
                 <Row>
                     <Col>
                         <Form.Group className="mb-3" controlId="dateId">
-                            <Form.Label>Date:</Form.Label>
+                            <Form.Label>Date:<sup>*</sup></Form.Label>
                             <Form.Control
                                 onChange={handleFormValues}
                                 type="number"
                                 name="date"
-                                value={originsValues.date} />
+                                value={originsValues.date}
+                                required />
                         </Form.Group>
                     </Col>
                     <Col>
                         <Form.Group className="mb-3" controlId="countryId">
-                            <Form.Label>Countries:</Form.Label>
+                            <Form.Label>Countries:<sup>*</sup></Form.Label>
                             <Form.Control
                                 onChange={handleFormValues}
                                 type="text"
                                 placeholder="Type countries separated with commas"
                                 name="countries"
-                                value={originsValues.country} />
+                                value={originsValues.country}
+                                required />
                         </Form.Group>
                     </Col>
                 </Row>
+
                 <Form.Group className="mb-3" controlId="descriptionId">
-                    <Form.Label>Description</Form.Label>
+                    <Form.Label>Description:<sup>*</sup></Form.Label>
                     <Form.Control
                         as="textarea"
                         onChange={handleFormValues}
@@ -101,27 +114,32 @@ const NewGenreForm = () => {
                         placeholder="Write a description (max 400 characters) "
                         maxLength={400}
                         name="description"
-                        value={formValues.description} />
+                        value={formValues.description}
+                        required />
                 </Form.Group>
+
                 <Form.Group className="mb-3" controlId="linkedBandsId">
-                    <Form.Label>Linked Bands</Form.Label>
+                    <Form.Label>Linked Bands:<sup>*</sup></Form.Label>
                     <Form.Control
                         onChange={handleFormValues}
                         type="text"
                         placeholder="Type the bands separated with commas"
                         name="linkedBands"
-                        value={formValues.linkedBands} />
+                        value={formValues.linkedBands}
+                        required />
                 </Form.Group>
+
                 <Form.Group className="mb-3" controlId="mainstreamId">
                     <Form.Check
                         onChange={handleFormValues}
                         type="checkbox"
-                        label="Is mainstream?"
+                        label="Is mainstream?:"
                         name="isMainstream"
                         checked={formValues.isMainstream} />
                 </Form.Group>
+
                 <Form.Group className="mb-3" controlId="parentGenreId">
-                    <Form.Label>Parent genre</Form.Label>
+                    <Form.Label>Parent genre:</Form.Label>
                     <Form.Control
                         onChange={handleFormValues}
                         type="text"
@@ -129,6 +147,7 @@ const NewGenreForm = () => {
                         name="parentGenre"
                         value={formValues.parentGenre} />
                 </Form.Group>
+
                 <Form.Group className="mb-3" controlId="childrenGenresId">
                     <Form.Label>Children genres</Form.Label>
                     <Form.Control
@@ -138,16 +157,19 @@ const NewGenreForm = () => {
                         name="childrenGenres"
                         value={formValues.childrenGenres} />
                 </Form.Group>
+
                 <Form.Group className="mb-3" controlId="">
                     <Form.Label>Images</Form.Label>
                     <Form.Control
                         onChange={handleFormValues}
                         type="text"
-                        placeholder="type URL"
+                        placeholder="type URLs separated with commas"
                         name="images"
                         value={formValues.images} />
                 </Form.Group>
+
                 <Button variant="outline-info" type="submit" className="shadow">Add Genre</Button>
+
             </Form>
         </div>
     )
