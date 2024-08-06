7bc8f134dca0ca4ab0a3ea440021027b5642c146
Sergio del Campo Puerta <sergio@sergioelmoreno.com>
2024-08-06 18:05:27 +0200

    Added Playlist functionality

--- src/components/PlaylistPlayer/PlaylistPlayer.jsx
+++ src/components/PlaylistPlayer/PlaylistPlayer.jsx
@@ -1,30 +1,53 @@
 import { useEffect, useState } from 'react'
-import { Card, ListGroup, Ratio } from 'react-bootstrap'
-import ReactPlayer from 'react-player/lazy'
+import { Badge, Button, Card, Col, ListGroup, Ratio, Row, Stack } from 'react-bootstrap'
+import ReactPlayer from 'react-player/youtube'
+import { Link } from 'react-router-dom'
 
 const PlaylistPlayer = ({ songsData, isLoading }) => {
-    const [playlist, setPlaylist] = useState(songsData)
+    const [playerState, setPlayerState] = useState({
+        url: null,
+        playing: false,
+        idx: 0
+    })
 
     useEffect(() => {
-
+        loadSong(0, false)
     }, [])
 
+    const loadSong = (idx, play) => {
+        setPlayerState({
+            url: songsData[idx].video,
+            playing: play,
+            idx
+        })
+    }
 
-    let playingIndex = 0
-    const playNextSong = () => {
-        playingIndex + 1
+    const pauseSong = idx => {
+        setPlayerState({ ...playerState, playing: false })
+    }
+
+    const handleEndedSong = () => {
+        playerState.idx === songsData.length - 1
+            ?
+            loadSong(0, true)
+            :
+            loadSong(playerState[idx] + 1, true)
 
     }
+
+    const { url, playing } = playerState
+
     return (
         <Card className='PlaylistPlayer'>
             <Card.Header>
                 <Ratio aspectRatio='16x9'>
                     <ReactPlayer
-                        url={playlist[playingIndex].video}
+                        controls
                         width='100%'
                         height='100%'
-                        onEnded={playNextSong} />
-
+                        url={url}
+                        playing={playing}
+                        onEnded={handleEndedSong} />
                 </Ratio>
             </Card.Header>
             <ListGroup variant='flush'>
@@ -33,12 +56,22 @@ const PlaylistPlayer = ({ songsData, isLoading }) => {
                         ?
                         <h1>Loading...</h1>
                         :
-                        playlist.map((song, idx) => {
+                        songsData.map((song, idx) => {
                             return <ListGroup.Item
-                                key={`playlist-${song.id}`}
-                                variant={idx === playingIndex && 'info'}
-                            >
-                                {song.title} - {song.albumTitle}
+                                key={`playlist-${idx}`}
+                                variant={idx === playerState.idx && 'info'} >
+                                <Stack direction='horizontal' gap={3} className='justify-content-between'>
+                                    <span className='w-auto'>
+                                        <Link to={`/songs/${song.id}`}>
+                                            {song.songBy.band}: {song.title}
+                                        </Link>
+                                        <hr className='my-2' />
+                                        <Badge bg='rebecca'>{song.albumTitle}</Badge>
+                                    </span>
+                                    <span >
+                                        <Button onClick={() => loadSong(idx, true)}>Play</Button>
+                                    </span>
+                                </Stack>
                             </ListGroup.Item>
                         })
                 }
