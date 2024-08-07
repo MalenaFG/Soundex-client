fd1d9faf773092bc8879ee60eb14b637a8a4df44
Sergio del Campo Puerta <sergio@sergioelmoreno.com>
2024-08-07 17:17:29 +0200

    Remove Header

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,3 +1,4 @@
+import './Navigation.css'
 import { Nav, Navbar, Container, Form } from "react-bootstrap"
 import { Link } from "react-router-dom"
 import SongFilter from "../SongFilter/SongFilter"
@@ -5,8 +6,9 @@ import soundex from "../../assets/soundex.svg"
 
 const Navigation = () => {
     return (
-        <Navbar collapseOnSelect expand="lg" className="bg-body-dark">
-            <Container>
+        <Navbar collapseOnSelect expand="lg" className="Navigation bg-body-dark">
+            <Container className='position-relative'>
+
                 <Navbar.Brand as={Link} to="/" className="d-flex align-items-center">
                     <img
                         src={soundex}
@@ -16,17 +18,23 @@ const Navigation = () => {
                         alt="Soundex" />
                     Soundex
                 </Navbar.Brand>
+
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+
                 <Navbar.Collapse id="responsive-navbar-nav">
+
                     <Nav className="me-auto" >
                         <Nav.Link to={"/genres"} as={Link} className="mx-md-2">Music genres</Nav.Link>
                         <Nav.Link to={"/songs"} as={Link} className="mx-md-2">Songs</Nav.Link>
                         <Nav.Link to={"/about"} as={Link} className="mx-md-2">About us</Nav.Link>
                     </Nav>
+
                     <Nav>
                         <SongFilter />
                     </Nav>
+
                 </Navbar.Collapse>
+
             </Container>
         </Navbar>
 
