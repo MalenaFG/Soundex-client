9ce7f3c8736eba147dd406a87b036a76e2a737bb
Sergio del Campo Puerta <sergio@sergioelmoreno.com>
2024-08-08 14:03:02 +0200

    Like button on GenreDetailsPage

--- src/pages/GenreDetailsPage/GenreDetailsPage.jsx
+++ src/pages/GenreDetailsPage/GenreDetailsPage.jsx
@@ -1,10 +1,11 @@
+import heartFill from '../../assets/heart-fill.svg'
+import heart from '../../assets/heart.svg'
 import axios from "axios"
 import { useEffect, useState, } from "react"
-import { Badge, Button, Col, Container, Row } from "react-bootstrap"
+import { Badge, Button, Col, Container, Image, Row } from "react-bootstrap"
 import { Link, useParams } from "react-router-dom"
 import CustomImagesCarousel from "../../components/CustomImagesCarousel/CustomImagesCarousel"
 import GenreDetailsSidebar from "../../components/GenreDetailsSidebar/GenreDetailsSidebar"
-import SongListCard from "../../components/SongListCard/SongListCard"
 import PlaylistPlayer from "../../components/PlaylistPlayer/PlaylistPlayer"
 
 const API_URL = "http://localhost:5005"
@@ -14,29 +15,52 @@ const GenreDetailsPage = () => {
     const { genreId } = useParams()
     const [genreData, setGenreData] = useState()
     const [songsData, setSongsData] = useState([])
+    const [like, setLike] = useState(false)
     const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        fetchGenreData()
+    }, [])
+
+    useEffect(() => {
+        genreData && putGenreData()
+    }, [like])
 
     const fetchGenreData = () => {
         axios
             .get(`${API_URL}/genres/${genreId}?_embed=songs`)
             .then(({ data }) => {
-                setGenreData(data)
+                const onlyGenreData = { ...data }
+                delete onlyGenreData.songs
+                setGenreData(onlyGenreData)
+                setLike(onlyGenreData.like)
                 data.songs.length > 0 && setSongsData(data.songs)
                 setIsLoading(false)
             })
             .catch(err => console.log(err))
     }
 
-    useEffect(() => {
-        fetchGenreData()
-    }, [])
+    const putGenreData = () => {
 
+        genreData.like = like
+        setGenreData(genreData)
+        const requestBody = { ...genreData }
+        axios
+            .put(`${API_URL}/genres/${genreId}`, requestBody)
+            .then()
+            .catch(err => console.log(err))
+
+    }
+
+    const handleLikes = () => {
+        setLike(!like)
+        putGenreData()
+    }
     const {
         name,
         description,
         images,
-        isMainstream,
-        rate
+        isMainstream
     } = isLoading || genreData
 
     return (
@@ -72,9 +96,9 @@ const GenreDetailsPage = () => {
                                                     }
                                                 </sup>
                                             </h1>
-                                            <Badge pill className="d-flex align-items-center fs-5">
-                                                Rating: {rate}
-                                            </Badge>
+
+                                            <Image onClick={handleLikes} src={like ? heartFill : heart} width={25} className='heart-like-icon' />
+
                                         </div>
                                         <hr />
                                         <p>{description}</p>
